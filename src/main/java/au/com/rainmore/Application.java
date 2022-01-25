0f08f11fbd6e9f3fa6f60240b0ad5b13bfd2a445
Felix Rong <rainmore24@gmail.com>
2022-01-25 22:34:16 +1100

    Init Java Application and BestTest

---
+++ src/main/java/au/com/rainmore/Application.java
@@ -0,0 +1,15 @@
+package au.com.rainmore;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    public static void main(String[] args) {
+
+    }
+
+}
