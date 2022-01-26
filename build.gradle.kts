f4405b92bfaaf0d8a6d53b401a297f1d18c0100c
Felix Rong <rainmore24@gmail.com>
2022-01-26 12:07:40 +1100

    Drop the redundant logging and enable the interaction from command line

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,11 +25,11 @@ application {
     mainClass.set(listOf(project.group.toString(), "Application").joinToString("."))
 }
 
-dependencies {
-    implementation("org.slf4j:slf4j-api:${project.properties["slf4j.version"]!!.toString()}")
-    implementation("ch.qos.logback:logback-classic:${project.properties["logback.version"]!!.toString()}")
-    implementation("ch.qos.logback:logback-core:${project.properties["logback.version"]!!.toString()}")
+tasks.run.configure {
+    standardInput = System.`in`
+}
 
+dependencies {
     testImplementation(platform("org.junit:junit-bom:${project.properties["junit.version"]!!.toString()}"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
